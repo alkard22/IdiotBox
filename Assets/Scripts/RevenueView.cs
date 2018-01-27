39484923f8b55badb18617a55d1ef31978d21578
Joh Robbins <joh.robbins@rea-group.com>
2018-01-27 12:46:54 +1100

    Added a Revenue Box

---
+++ Assets/Scripts/RevenueView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RevenueView : MonoBehaviour
+{
+    public Text revenue;
+    public GameObject trendIcon;
+
+    private int currentRevenue = 0;
+
+    public void UpdateRevenue(int value)
+    {
+        trendIcon.SetActive(true);
+        if(currentRevenue < value) {
+            revenue.text = string.Format("{0:C0}", value);
+            trendIcon.GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, 180f);
+        } else if (currentRevenue > value) {
+            revenue.text = string.Format("{0:C0}", value);
+            trendIcon.GetComponent<RectTransform>().rotation = Quaternion.identity;
+        } else {
+            trendIcon.SetActive(false);
+        }
+
+        currentRevenue = value;
+    }
+
+}
