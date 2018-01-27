e9200a6a49e869ecc869e16195da78806bded0ca
Erica <ericathegreat@gmail.com>
2018-01-27 19:23:59 +1100

    Week counter

---
+++ Assets/Scripts/WeekCounter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeekCounter : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<UnityEngine.UI.Text>().text = "" + GameState.current.turn;
+	}
+}
