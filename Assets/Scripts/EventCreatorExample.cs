4d99355664d8245897de44c232904619b9f38828
Joh Robbins <joh.robbins@rea-group.com>
2018-01-27 11:19:13 +1100

    Added Weekly Event

---
+++ Assets/Scripts/EventCreatorExample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EventCreatorExample : MonoBehaviour
+{
+    public WeeklyEvent weeklyEvent;
+
+    // Use this for initialization
+    void Start()
+    {
+        EventData data = new EventData();
+        data.title = "Welcome to the Game!";
+        data.message = "Today is the day you become the owner of multi-million dollar TV Studio. Your goal is to grow your viewer audience and dominate the markert.";
+        data.optionOne = "Okay";
+        data.optionTwo = "Nooooooooooooooooo!";
+
+        weeklyEvent.NewEvent(data);
+    }
+}
---
+++ Assets/Scripts/WeeklyEvent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public struct EventData
+{
+    public string title;
+    public string message;
+    public string optionOne;
+    public string optionTwo;
+}
+
+public class WeeklyEvent : MonoBehaviour
+{
+    public Text titleBox;
+    public Text messageBox;
+    public Text optionOneText;
+    public GameObject optionOneButton;
+    public Text optionTwoText;
+    public GameObject optionTwoButton;
+
+    //TODO: handle a delegate to change some effect depending on user option selected
+
+    public void NewEvent(EventData data)
+    {
+        this.gameObject.SetActive(true);
+        bool secondOption = false;
+
+        titleBox.text = data.title;
+        messageBox.text = data.message;
+        optionOneText.text = data.optionOne;
+
+        if(data.optionOne != null) {
+            optionTwoText.text = data.optionTwo;
+            secondOption = true;
+        }
+
+        optionTwoButton.SetActive(secondOption);
+    }
+}
