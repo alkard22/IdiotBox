70dd69debf501dc13700074975072059cee7cc04
Erica <ericathegreat@gmail.com>
2018-01-27 18:08:43 +1100

    Avatar Generation for TV Shows

---
+++ Assets/Scripts/AvatarConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarConfig : MonoBehaviour {
+
+    public Color[] colors;
+    public Font[] fonts;
+
+}
---
+++ Assets/Scripts/ShowAvatar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
+using UnityEngine.UI;
+
+public class ShowAvatar : MonoBehaviour {
+
+    public AvatarConfig config;
+    public string title;
+    private Image background;
+    private Text word;
+
+    // Use this for initialization
+    void Start ()
+    {
+        background = this.gameObject.transform.GetChild(0).gameObject.GetComponent<UnityEngine.UI.Image>();
+        word = this.gameObject.transform.GetChild(1).gameObject.GetComponent<UnityEngine.UI.Text>();
+
+        GenerateAvatar();
+    }
+	
+    public void GenerateAvatar()
+    {
+        byte[] hash = MD5.Create().ComputeHash(System.Text.Encoding.ASCII.GetBytes(title));
+        int hashed = Mathf.Abs(System.BitConverter.ToInt32(hash, 0));
+
+        background.color = config.colors[hashed % config.colors.Length];
+        word.font = config.fonts[hashed % config.fonts.Length];
+        word.text = "" + title.ToCharArray()[0];
+    }
+}
