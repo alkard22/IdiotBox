2a7d572811a404f8b053a1b6fb1d17f4ed7b921f
Erica <ericathegreat@gmail.com>
2018-01-27 21:06:31 +1100

    Styling RevenueView

--- Assets/Scripts/RevenueView.cs
+++ Assets/Scripts/RevenueView.cs
@@ -7,6 +7,9 @@ public class RevenueView : MonoBehaviour
     public Text revenue;
     public GameObject trendIcon;
 
+    public Color up;
+    public Color down;
+
     private int currentRevenue = 0;
 
     public void UpdateRevenue(int value)
@@ -15,9 +18,11 @@ public class RevenueView : MonoBehaviour
         if(currentRevenue < value) {
             revenue.text = string.Format("{0:C0}", value);
             trendIcon.GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, 180f);
+            trendIcon.GetComponent<Image>().color = down;
         } else if (currentRevenue > value) {
             revenue.text = string.Format("{0:C0}", value);
             trendIcon.GetComponent<RectTransform>().rotation = Quaternion.identity;
+            trendIcon.GetComponent<Image>().color = up;
         } else {
             trendIcon.SetActive(false);
         }
