bcc3c0cb765c5b6d0a97967084b7b7fee99cf3ec
Michael Vigilante <vigilante.michael@gmail.com>
2018-01-28 10:07:32 +1100

    Add the WeeklyActionController Script >_>

---
+++ Assets/Scripts/WeeklyActionController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeeklyActionController : MonoBehaviour {
+	public void EndTurn()
+	{
+		GameState.NextTurn ();
+	}
+}
