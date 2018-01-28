c40f64635a068f694a72d993dee701081a0ce589
Joh Robbins <joh.robbins@rea-group.com>
2018-01-28 14:12:03 +1100

    Added background animation

---
+++ Assets/Scripts/AnimateBackGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateBackGround : MonoBehaviour
+{
+    public List<Sprite> backgroundAnimations;
+    private int animationFrame = 0;
+
+    void Start()
+    {
+        InvokeRepeating("ChangeBackGroundImage", 0f, 0.8f);  //1s delay, repeat every 1s
+    }
+
+    private void ChangeBackGroundImage()
+    {
+        if(animationFrame >= backgroundAnimations.Count) {
+            animationFrame = 0;
+        }
+        this.GetComponent<Image>().sprite = backgroundAnimations[animationFrame++];
+    }
+
+}
