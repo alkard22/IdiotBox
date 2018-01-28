813fb69311bb0e453ae93a013617c60357577198
Joh Robbins <joh.robbins@rea-group.com>
2018-01-28 13:52:48 +1100

    Added Exit button

---
+++ Assets/AnimateBackGround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateBackGround : MonoBehaviour
+{
+    //public List<Sprite> backgroundAnimations;
+
+    //void Start()
+    //{
+    //    InvokeRepeating(ChangeBackGroundImage, 1f, 1f);  //1s delay, repeat every 1s
+    //}
+
+    //private void ChangeBackGroundImage()
+    //{
+
+    //}
+
+}
